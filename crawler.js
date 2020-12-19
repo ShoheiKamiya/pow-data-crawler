d2f006f0e52d2833445c414db1a0044e0b6eea85
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-12-19 09:57:51 +0900

    fetch rusutsu data

--- crawler.js
+++ crawler.js
@@ -4,8 +4,30 @@ import { mountains } from './mountains.js';
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
   await page.goto(mountains[0].url);
-  await page.screenshot({ path: 'example.png' });
+  const depthElements = await page.$x(mountains[0].depthXpath);
+  const snowfallElements = await page.$x(mountains[0].snowfallXpath);
+  const tempretureElements = await page.$x(mountains[0].tempretureXpath);
+  const updatedElements = await page.$x(mountains[0].updatedXpath);
+
+  const depth = await (
+    await depthElements[0].getProperty('textContent')
+  ).jsonValue();
+  const snowfall = await (
+    await snowfallElements[0].getProperty('textContent')
+  ).jsonValue();
+  const tempreture = await (
+    await tempretureElements[0].getProperty('textContent')
+  ).jsonValue();
+  const updated = await (
+    await updatedElements[0].getProperty('textContent')
+  ).jsonValue();
+
+  console.log({ depth });
+  console.log({ snowfall });
+  console.log({ tempreture });
+  console.log({ updated });
 
   await browser.close();
 })();
--- mountains.js
+++ mountains.js
@@ -2,8 +2,9 @@ export const mountains = [
   {
     mountainId: 'rusutsu',
     url: 'https://rusutsu.com/snow-and-weather-report/',
-    snowfall: '/html/body/main/article[4]/section/div[1]/div[2]/dl[2]/dd',
-    depth: '/html/body/main/article[4]/section/div[1]/div[2]/dl[1]/dd',
-    tempreture: '/html/body/main/article[4]/section/div[1]/div[1]/p[2]',
-  }
+    snowfallXpath: '/html/body/main/article[4]/section/div[1]/div[2]/dl[2]/dd',
+    depthXpath: '/html/body/main/article[4]/section/div[1]/div[2]/dl[1]/dd',
+    tempretureXpath: '/html/body/main/article[4]/section/div[1]/div[1]/p[2]',
+    updatedXpath: '/html/body/main/article[4]/section/h4'
+  },
 ]
