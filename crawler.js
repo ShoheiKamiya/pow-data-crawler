be035ba6d9233a8416f790d9dd43d79ff2f0bb68
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-12-19 19:26:17 +0900

    refactoring

--- crawler.js
+++ crawler.js
@@ -1,37 +1,31 @@
 import puppeteer from 'puppeteer';
 import { mountains } from './mountains.js';
 
+const fetchWithXpath = async (page, xpath) => {
+  if (!xpath) {
+    return null;
+  }
+  const elements = await page.$x(xpath);
+  return await (await elements[0].getProperty('textContent')).jsonValue();
+};
+
 (async () => {
   const browser = await puppeteer.launch({ ignoreHTTPSErrors: true });
   const page = await browser.newPage();
 
   for await (let mountain of mountains) {
     await page.goto(mountain.url, { waitUntil: 'networkidle0' });
+
     console.log(`<< ${mountain.id} >>`);
+    const snowfall = await fetchWithXpath(page, mountain.snowfallXpath);
+    const depth = await fetchWithXpath(page, mountain.depthXpath);
+    const tempreture = await fetchWithXpath(page, mountain.tempretureXpath);
+    const updated = await fetchWithXpath(page, mountain.updatedXpath);
 
-    if (mountain.snowfallXpath) {
-      const snowfallElements = await page.$x(mountain.snowfallXpath);
-      const snowfall = await (await snowfallElements[0].getProperty('textContent')).jsonValue();
-      console.log({ snowfall });
-    }
-
-    if (mountain.depthXpath) {
-      const depthElements = await page.$x(mountain.depthXpath);
-      const depth = await (await depthElements[0].getProperty('textContent')).jsonValue();
-      console.log({ depth });
-    }
-
-    if (mountain.tempretureXpath) {
-      const tempretureElements = await page.$x(mountain.tempretureXpath);
-      const tempreture = await (await tempretureElements[0].getProperty('textContent')).jsonValue();
-      console.log({ tempreture });
-    }
-
-    if (mountain.tempretureXpath) {
-      const updatedElements = await page.$x(mountain.updatedXpath);
-      const updated = await (await updatedElements[0].getProperty('textContent')).jsonValue();
-      console.log({ updated });
-    }
+    console.log({ snowfall });
+    console.log({ depth });
+    console.log({ tempreture });
+    console.log({ updated });
   }
 
   await browser.close();
