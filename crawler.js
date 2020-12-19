384f1f9af3030cf1f87db138f3fe93e40d0c74fd
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-12-19 17:49:34 +0900

    some mountains

--- crawler.js
+++ crawler.js
@@ -2,24 +2,27 @@ import puppeteer from 'puppeteer';
 import { mountains } from './mountains.js';
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ ignoreHTTPSErrors: true });
   const page = await browser.newPage();
 
-  await page.goto(mountains[0].url);
-  const depthElements = await page.$x(mountains[0].depthXpath);
-  const snowfallElements = await page.$x(mountains[0].snowfallXpath);
-  const tempretureElements = await page.$x(mountains[0].tempretureXpath);
-  const updatedElements = await page.$x(mountains[0].updatedXpath);
+  for await (let mountain of mountains) {
+    await page.goto(mountain.url);
+    const depthElements = await page.$x(mountain.depthXpath);
+    const snowfallElements = await page.$x(mountain.snowfallXpath);
+    const tempretureElements = await page.$x(mountain.tempretureXpath);
+    const updatedElements = await page.$x(mountain.updatedXpath);
 
-  const depth = await (await depthElements[0].getProperty('textContent')).jsonValue();
-  const snowfall = await (await snowfallElements[0].getProperty('textContent')).jsonValue();
-  const tempreture = await (await tempretureElements[0].getProperty('textContent')).jsonValue();
-  const updated = await (await updatedElements[0].getProperty('textContent')).jsonValue();
+    const depth = await (await depthElements[0].getProperty('textContent')).jsonValue();
+    const snowfall = await (await snowfallElements[0].getProperty('textContent')).jsonValue();
+    const tempreture = await (await tempretureElements[0].getProperty('textContent')).jsonValue();
+    const updated = await (await updatedElements[0].getProperty('textContent')).jsonValue();
 
-  console.log({ depth });
-  console.log({ snowfall });
-  console.log({ tempreture });
-  console.log({ updated });
+    console.log(`<< ${mountain.id} >>`);
+    console.log({ depth });
+    console.log({ snowfall });
+    console.log({ tempreture });
+    console.log({ updated });
+  }
 
   await browser.close();
 })();
--- mountains.js
+++ mountains.js
@@ -1,10 +1,26 @@
 export const mountains = [
   {
-    mountainId: 'rusutsu',
+    id: 'rusutsu',
     url: 'https://rusutsu.com/snow-and-weather-report/',
     snowfallXpath: '/html/body/main/article[4]/section/div[1]/div[2]/dl[2]/dd',
     depthXpath: '/html/body/main/article[4]/section/div[1]/div[2]/dl[1]/dd',
     tempretureXpath: '/html/body/main/article[4]/section/div[1]/div[1]/p[2]',
     updatedXpath: '/html/body/main/article[4]/section/h4'
   },
+  {
+    id: 'nozawa',
+    url: 'http://nozawaski.com/winter/course/',
+    snowfallXpath: '//*[@id="now_gelande"]/div/div[1]/table/tbody/tr[5]/td',
+    depthXpath: '//*[@id="now_gelande"]/div/div[1]/table/tbody/tr[4]/td',
+    tempretureXpath: '//*[@id="now_gelande"]/div/div[1]/table/tbody/tr[2]/td',
+    updatedXpath: '//*[@id="now_gelande"]/div/div[1]/p'
+  },
+  // {
+  //   id: '',
+  //   url: '',
+  //   snowfallXpath: '',
+  //   depthXpath: '',
+  //   tempretureXpath: '',
+  //   updatedXpath: ''
+  // },
 ]
