19c84d40131f5e7a02fde2eaa6af36c863b11ade
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-12-20 00:10:57 +0900

    add GrandHirafu class

---
+++ mountains/GrandHirafu.js
@@ -0,0 +1,46 @@
+export class GrandHirafu {
+  constructor(snowfallStr, depthStr, temperatureStr, updatedStr) {
+    this.snowfallStr = snowfallStr;
+    this.depthStr = depthStr;
+    this.temperatureStr = temperatureStr;
+    this.updatedStr = updatedStr;
+  }
+
+  static get ID() {
+    return 'grand_hirafu';
+  }
+  static get URL() {
+    return 'https://www.grand-hirafu.jp/winter/gelande/business_hours/'
+  }
+  static get SELECTORS() {
+    return {
+      snowfall: null,
+      depth: '//*[@id="yWeather_8KuHUsi4MLcfzRXG"]/td[5]',
+      temperature: '//*[@id="yWeather_8KuHUsi4MLcfzRXG"]/td[3]',
+      updated: '//*[@id="weather_update_time"]',
+    };
+  }
+
+  get snowfall() {
+    return null;
+  }
+  get depth() {
+    return Number(this.depthStr.match(/\d+/, ''));
+  }
+  get temperature() {
+    return Number(this.temperatureStr.replace(' °C', ''))
+  }
+  get updated() {
+    // TODO: yyyy-mm-ddの形式に変換する
+    return this.updatedStr;
+  }
+
+  get params() {
+    return {
+      snowfall: this.snowfall,
+      depth: this.depth,
+      temperature: this.temperature,
+      updated: this.updated,
+    }
+  }
+}
