ea092dbc6d6d3fe8c52ceaaeb30872917e6905ad
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-12-23 23:13:41 +0900

    rm console.log

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,6 +2,5 @@ import { formatDate } from '../utils';
 
 test('test Date', () => {
   const date = new Date(2020, 12 - 1, 20);
-  console.log(date);
   expect(formatDate(date)).toBe('2020-12-20');
 });
