d996400d353b31e6af570538f49b470b80c18162
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-12-24 23:54:12 +0900

    add utils test

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,6 +1,11 @@
 import { formatDate } from '../utils';
 
-test('test Date', () => {
-  const date = new Date(2020, 12 - 1, 20);
-  expect(formatDate(date)).toBe('2020-12-20');
+test('フォーマットすること', () => {
+  const date1 = new Date(2020, 12 - 1, 20);
+  expect(formatDate(date1)).toBe('2020-12-20');
+});
+
+test('0埋めすること', () => {
+  const date1 = new Date(2020, 3 - 1, 10);
+  expect(formatDate(date1)).toBe('2020-03-10');
 });
