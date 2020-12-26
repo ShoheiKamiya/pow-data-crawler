7a5d1df5ccbbdfe9d576d8b4b4dfb03de4c0b41a
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-12-26 18:06:34 +0900

    add babel-loader

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -6,4 +6,19 @@ module.exports = {
     filename: 'sample.js',
   },
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+            plugins: ['@babel/plugin-proposal-async-generator-functions'],
+          },
+        },
+      },
+    ],
+  },
 };
