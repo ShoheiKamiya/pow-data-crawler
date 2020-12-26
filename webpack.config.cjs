22f352408e27f494a68683f96f2468b3993caff2
ShoheiKamiya <49185403+ShoheiKamiya@users.noreply.github.com>
2020-12-26 18:26:56 +0900

    POW-16 add webpack and babel (#17)
    * install babel-cli
    
    * install babel plugin to compile 'for await'
    
    * not babel-cli but @babel/cli
    
    * update babel config
    
    * install webpack
    
    * configure 'yarn build'
    
    * update webpack config
    
    * add babel-loader
    
    * fix webpack configure

---
+++ webpack.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './index.js',
+  target: 'node',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+            plugins: ['@babel/plugin-proposal-async-generator-functions'],
+          },
+        },
+      },
+    ],
+  },
+};
