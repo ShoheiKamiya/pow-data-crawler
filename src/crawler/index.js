fb9c3408cba662d5910d9b48f12760ac0c13ec87
ShoheiKamiya <49185403+ShoheiKamiya@users.noreply.github.com>
2020-12-29 22:25:33 +0900

    POW-3 lambdaを並列実行する (#26)
    * execute on lambda
    
    * mv utils.js

---
+++ src/crawler/index.js
@@ -0,0 +1,40 @@
+exports.handler = async(event) => {
+  const className = event.mountain
+  const mountain = require(`./mountains/${className}.js`)[className]
+  const chromium = require('chrome-aws-lambda');
+
+  const fetchWithXpath = async(page, xpath) => {
+    if (!xpath) {
+      return null;
+    }
+    const elements = await page.$x(xpath);
+
+    return await (await elements[0].getProperty('textContent')).jsonValue();
+  };
+
+  // puppeteer
+  const browser = await chromium.puppeteer.launch({
+    args: [
+      ...chromium.args,
+      '--single-process',
+    ],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true
+  });
+  const page = await browser.newPage();
+
+  await page.goto(mountain.URL, {
+    waitUntil: 'networkidle0'
+  });
+  const snowfall = await fetchWithXpath(page, mountain.SELECTORS.snowfall);
+  const depth = await fetchWithXpath(page, mountain.SELECTORS.depth);
+  const temperature = await fetchWithXpath(page, mountain.SELECTORS.temperature);
+  const updated = await fetchWithXpath(page, mountain.SELECTORS.updated);
+  const m = new mountain(snowfall, depth, temperature, updated);
+
+  browser.close();
+
+  return m.params;
+};
